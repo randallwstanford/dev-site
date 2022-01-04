74397db45ba3e1b914f44d5e407e68c5b2831315
randallwstanford <randallwstanford@gmail.com>
2022-01-04 16:28:32 -0700

    deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import node from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: node({ env: { port: process.env.PORT } }),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
