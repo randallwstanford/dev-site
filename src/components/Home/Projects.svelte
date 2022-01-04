64a09438c98eef3470ca052d5d42eeb7d69c54cf
randallwstanford <randallwstanford@gmail.com>
2022-01-04 16:12:34 -0700

    Refactor project mapping logic.

--- src/components/Home/Projects.svelte
+++ src/components/Home/Projects.svelte
@@ -1,30 +1,30 @@
 <script>
-var projects = [
-    {id: 1, name: 'Altier API', img: "", description: "API to handle all routes for an e-commerce site."},
-    {id: 2, name: 'Cocktail Builder', img: "https://user-images.githubusercontent.com/83252804/138566931-80b8c442-3cc7-4ac1-8ee0-0b4424aa7d54.png", description: "Single page application that shows you what cocktails you are able to build with the product you have on hand."},
-    {id: 3, name: 'Project Catwalk', img: "", description: "A client requested e-commerce site redesign."}
-  ];
+  import Loading from './Loading.svelte'
+
+  var projects = [
+      {id: 2, name: 'Cocktail Builder', img: "https://user-images.githubusercontent.com/83252804/138566931-80b8c442-3cc7-4ac1-8ee0-0b4424aa7d54.png", description: "Single page application that shows you what cocktails you are able to build with the product you have on hand."},
+      {id: 1, name: 'Altier API', img: "", description: "API to handle all routes for an e-commerce site."},
+      {id: 3, name: 'Project Catwalk', img: "", description: "A client requested e-commerce site redesign."}
+    ];
 </script>
 
 {#each projects as project}
   <div class="project">
     <div class="title">{project.name}</div>
     <div class="description">{project.description}</div>
-    {#if project.img}
-      <img class="img" src={project.img} alt=""/>
-    {:else}
-      <div class="loading">Loading image...</div>
-    {/if}
+    {#if project.img} <img src={project.img} alt=""/>
+    {:else}<Loading />{/if}
   </div>
 {/each}
 
 <style>
   .project {
-    float: left;
-    margin-left: 5rem;
-    margin-right: 7.5rem;
-    height: 40rem;
-    width: 30rem;
+    margin: 0 auto;
+    height: 35rem;
+    width: 50rem;
+    display: block;
+    padding-bottom: 1rem;
+    border-bottom: .1rem solid black;
   }
   .title {
     margin-top: 1rem;
@@ -36,14 +36,9 @@ var projects = [
     text-align: center;
     font-size: 1.25rem;
   }
-  .loading {
-    margin-top: 10rem;
-    font-weight: bold;
-    text-align: center;
-    font-size: 2rem;
-  }
-  .img {
-    margin-top: 5rem;
-    height: 15rem;
+  img {
+    margin-top: 1rem;
+    width: 100%;
   }
 </style>
+
