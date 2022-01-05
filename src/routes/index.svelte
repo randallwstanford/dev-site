0d2a694c845b7bf44de3b583f330d59f7cca18f7
randallwstanford <randallwstanford@gmail.com>
2022-01-04 21:46:22 -0700

    Adds contact route.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,14 @@
   import { Router, Route } from 'svelte-routing';
   import Home from '../components/Home/Home.svelte';
   import About from '../components/About/About.svelte';
-
+  import Contact from '../components/Contact/Contact.svelte';
   export let url = '';
 </script>
 
 <Router {url}>
   <div class="app">
     <Route path="about" component={About} />
+    <Route path="contact" component={Contact} />
     <Route path="/" component={Home} />
   </div>
 </Router>
