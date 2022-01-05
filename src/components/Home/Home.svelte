e3a957cbf52153ff4045c10efd3e2dc0533737c8
randallwstanford <randallwstanford@gmail.com>
2022-01-04 21:47:04 -0700

    Changes home styling.

--- src/components/Home/Home.svelte
+++ src/components/Home/Home.svelte
@@ -3,19 +3,20 @@
 	import Body from "./Body.svelte";
 </script>
 
-<style>
-	#container {
-    --eton-blue: #93C0A4;
-    overflow: hidden;
-    margin: 0 auto;
-    width: 90rem;
-    height: 75rem;
-    display: flex;
-    flex-direction: column;
-  }
-</style>
 
 <div id="container">
   <Nav />
   <Body />
 </div>
+
+<style>
+  #container {
+    --eton-blue: #93C0A4;
+    overflow: hidden;
+    margin: 0 auto;
+    width: 90rem;
+    height: 150rem;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
