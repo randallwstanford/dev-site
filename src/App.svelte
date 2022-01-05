bb3f31280df4c08abcf831bcb9de3f9f6401918c
randallwstanford <randallwstanford@gmail.com>
2022-01-05 12:28:41 -0700

    Adds root css variables.

--- src/App.svelte
+++ src/App.svelte
@@ -6,3 +6,12 @@
 <main>
   <Router />
 </main>
+
+<style>
+  :root {
+    --light: #A1A6B4;
+    --scrollbarWidth: 14px;
+    --scrollbarBg: rgb(0, 0, 0);
+    --scrollbarThumb: var(--light);
+  }
+</style>
