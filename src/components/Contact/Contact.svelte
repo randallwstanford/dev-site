7a6bc3b7c30df370d5fdcbfaa4632e68b11e854f
randallwstanford <randallwstanford@gmail.com>
2022-01-04 21:46:03 -0700

    Adds contact page with no styling yet.

---
+++ src/components/Contact/Contact.svelte
@@ -0,0 +1,12 @@
+<style>
+
+</style>
+
+<div>
+  <form>
+    <input type="text" placeholder="name" />
+    <input type="text" placeholder="email" />
+    <input type="text" placeholder="subject" />
+    <input type="text" placeholder="message" />
+  </form>
+</div>
