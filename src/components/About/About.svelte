8a983007b5463c5c178aa18144c4c98e21588db3
randallwstanford <randallwstanford@gmail.com>
2022-01-05 12:29:10 -0700

    Creates about me styling with temporary lorem ipsum.

--- src/components/About/About.svelte
+++ src/components/About/About.svelte
@@ -1,7 +1,74 @@
-<style>
+<script>
+  import Nav from '../Nav.svelte';
+</script>
 
-</style>
-
-<div>
-  this is the about me
+<div id="container">
+  <Nav />
+  <div class="body">
+    <div class="content">
+      <h1>About me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus feugiat in ante metus. Curabitur vitae nunc sed velit. Nunc sed blandit libero volutpat. Eu nisl nunc mi ipsum. Lacus vestibulum sed arcu non. A arcu cursus vitae congue mauris rhoncus. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. Sit amet purus gravida quis blandit turpis cursus in. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Eget est lorem ipsum dolor sit amet. Quam lacus suspendisse faucibus interdum. Ipsum a arcu cursus vitae congue. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Ipsum nunc aliquet bibendum enim facilisis gravida neque. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer. Mus mauris vitae ultricies leo integer malesuada nunc vel risus. Donec enim diam vulputate ut pharetra. Eu ultrices vitae auctor eu augue ut.
+      </p>
+      <p>
+        Neque vitae tempus quam pellentesque nec nam aliquam. At quis risus sed vulputate odio ut enim blandit. Non nisi est sit amet facilisis magna. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Commodo ullamcorper a lacus vestibulum sed arcu. Nisl rhoncus mattis rhoncus urna neque. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Pretium fusce id velit ut. Risus in hendrerit gravida rutrum quisque. Semper viverra nam libero justo. Proin sed libero enim sed faucibus turpis in eu mi. Sit amet volutpat consequat mauris nunc congue nisi. Tempus iaculis urna id volutpat lacus laoreet non. Ipsum a arcu cursus vitae congue mauris rhoncus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus feugiat in ante metus. Curabitur vitae nunc sed velit. Nunc sed blandit libero volutpat. Eu nisl nunc mi ipsum. Lacus vestibulum sed arcu non. A arcu cursus vitae congue mauris rhoncus. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. Sit amet purus gravida quis blandit turpis cursus in. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Eget est lorem ipsum dolor sit amet. Quam lacus suspendisse faucibus interdum. Ipsum a arcu cursus vitae congue. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Ipsum nunc aliquet bibendum enim facilisis gravida neque. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer. Mus mauris vitae ultricies leo integer malesuada nunc vel risus. Donec enim diam vulputate ut pharetra. Eu ultrices vitae auctor eu augue ut.
+      </p>
+      <p>
+        Neque vitae tempus quam pellentesque nec nam aliquam. At quis risus sed vulputate odio ut enim blandit. Non nisi est sit amet facilisis magna. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Commodo ullamcorper a lacus vestibulum sed arcu. Nisl rhoncus mattis rhoncus urna neque. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Pretium fusce id velit ut. Risus in hendrerit gravida rutrum quisque. Semper viverra nam libero justo. Proin sed libero enim sed faucibus turpis in eu mi. Sit amet volutpat consequat mauris nunc congue nisi. Tempus iaculis urna id volutpat lacus laoreet non. Ipsum a arcu cursus vitae congue mauris rhoncus.
+      </p>
+    </div>
+  </div>
 </div>
+
+<style>
+  #container {
+    overflow: hidden;
+    margin: 0 auto;
+    width: 90rem;
+    height: 150rem;
+    display: flex;
+    flex-direction: column;
+  }
+  h1 {
+    margin-bottom: 2rem;
+  }
+  p {
+    font-size: large;
+  }
+  .content {
+    margin: 2rem;
+  }
+  .body{
+    margin: 0 auto;
+    margin-top: 3rem;
+    font-family: 'Comfortaa', cursive;
+    height: 50rem;
+    width: 80rem;
+    background-color: #A1A6B4;
+    overflow: auto;
+    overflow-x: hidden;
+  }
+  /* @media only screen and (max-width: 1024px) {
+    .body {
+      height: 65rem;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .body {
+      height: 90rem;
+    }
+  }
+  @media only screen and (max-width: 425px) {
+    .body {
+      height: 100%;
+    }
+  }
+  @media only screen and (max-width: 320px) {
+    .body {
+      width: 90%;
+    }
+  } */
+</style>
