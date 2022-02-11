b8d6a4c00dac7f5d7dd5d01de75ddb503169a79b
randallwstanford <randallwstanford@gmail.com>
2022-02-11 14:54:00 -0700

    Adds description.

--- src/components/Home/Projects.svelte
+++ src/components/Home/Projects.svelte
@@ -4,7 +4,7 @@
   var projects = [
       {name: 'Bike Trails', img: "https://user-images.githubusercontent.com/83252804/152218263-93d6f53b-b9df-43b3-ad73-9c144fe262d3.png", description: "Single page application that finds bike trails and friendly bike roads using React, Google Maps API, and react-google-maps/api library."},
       {name: 'Huntr', img: "https://user-images.githubusercontent.com/83252804/146260695-79c508bf-f886-445a-8410-78d47f6b68aa.png", description: "Ride share application MVP built in one week featuring Google Maps API, userdashboard, messaging, and authentication."},
-      {name: 'Movie Finder', img: "https://user-images.githubusercontent.com/83252804/153654406-a63387c9-7924-4537-8116-1acbf7df860e.png", description: ""},
+      {name: 'Movie Finder', img: "https://user-images.githubusercontent.com/83252804/153654406-a63387c9-7924-4537-8116-1acbf7df860e.png", description: "Can't find anything to watch? This Single page app was built to help search popular movies, search random movies in the entire database, or find movies based on a keyword."},
       {name: 'Cocktail Builder', img: "https://user-images.githubusercontent.com/83252804/152252511-225f13a4-de3d-4d43-8dcb-6ca5b263cd0c.png", description: "Single page application that shows you what cocktails you are able to build with the product you have on hand."},
     ];
 </script>
